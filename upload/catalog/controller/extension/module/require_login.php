a5463392115f01a0f0fb04c9bf221bb95c130a9d
Emmanuel Zerna <emzer214@gmail.com>
2019-04-11 16:02:58 +0800

    fix session error due error caused by route undefined or null when in homepage without route value.

--- upload/catalog/controller/extension/module/require_login.php
+++ upload/catalog/controller/extension/module/require_login.php
@@ -1,25 +1,28 @@
 <?php
-
 class ControllerExtensionModuleRequireLogin extends Controller {
-    
     public function __construct($registry) {
         parent::__construct($registry);
+
+        $this->load->model('setting/setting');
         $setting = $this->model_setting_setting->getSetting('module_require_login');
 
         if ($setting['module_require_login_enabled']==1)
         {
         
             $allowed[] = $setting['module_require_login_redirect_page'];
-            foreach($setting['module_require_login_allowed_pages'] as $page)
-            {
+            foreach($setting['module_require_login_allowed_pages'] as $page){
                 $allowed[]=$page;
             }
-            if (!in_array($this->request->get['route'],$allowed) && !$this->customer->isLogged())
+
+            $route = '';
+            if(isset($this->request->get['route']))
             {
-                $this->response->redirect($this->url->link('account/login','', ''));
+                $route = $this->request->get['route'];
+            }
+
+            if (!in_array($route,$allowed) && !$this->customer->isLogged()){
+               $this->response->redirect($this->url->link('account/login','',''));
             }
         }
     }
-
-    
 }
